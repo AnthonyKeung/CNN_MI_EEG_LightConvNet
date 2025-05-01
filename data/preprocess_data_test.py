47f6d0ff9274b04de7d9f409cdff548f5f2566c2
AnthonyKeung <anthonykeung2468@gmail.com>
2025-05-01 17:12:58 +0100

    Working fir 2a dataset about to change for 2b

---
+++ data/preprocess_data_test.py
@@ -0,0 +1,60 @@
+import mne
+from mne import event
+from mne import epochs
+from mne import label
+import numpy as np
+import scipy.io as sio
+import os
+
+# Todo adjust the save path
+data_path = 'c:\Bath\Year 5\FYP\EEG_BCI_Deep_Learning\BCI_IV_2a'
+data_files = ['A0'+str(i)+'E.gdf' for i in range(1,10)]
+
+label_path = 'C:\Bath\Year 5\FYP\EEG_BCI_Deep_Learning\BCI_IV_2a_mat'
+label_files = ['A0'+str(i)+'E.mat' for i in range(1,10)]
+
+save_path = 'dataset/bci_iv_2a/raw'
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+event_description = {'783':"CueUnknown"}
+
+# for file in data_files:
+#     raw_data = mne.io.read_raw_gdf(os.path.join(data_path, file), preload=True, verbose=False)
+
+#     raw_events, all_event_id = mne.events_from_annotations(raw_data)
+
+#     raw_data = mne.io.RawArray(raw_data.get_data()*1e6, raw_data.info)
+
+#     raw_data.info['bads'] += ['EOG-left', 'EOG-central', 'EOG-right']
+
+#     test_picks = mne.pick_types(raw_data.info, eeg=True, exclude='bads')
+
+#     tmin, tmax = 0, 4
+
+#     # Unknown = 783
+#     event_id = dict()
+#     for event in all_event_id:
+#         if event in event_description:
+#             event_id[event] = all_event_id[event]
+
+#     raw_epochs = mne.Epochs(raw_data, raw_events, event_id, tmin, tmax, proj=True, picks=test_picks, baseline=None, preload=True)
+
+#     data = raw_epochs.get_data() # [n_epochs, n_channels, n_times]
+#     data = data[:, :, :-1]
+
+#     np.save(os.path.join(save_path, file[:-4]+'_data.npy'), data)
+
+y = []
+for file in label_files:
+    mat_data = sio.loadmat(os.path.join(label_path, file))
+    number_of_sessions = mat_data["data"].shape[1]
+    print("Number of sessions:", number_of_sessions)
+
+    # get data in      X : array, shape (n_epochs, n_channels, n_times) format 
+    for session_num in range(number_of_sessions):
+        labels = mat_data["data"][0][session_num][0][0][2].flatten()
+        y.extend(labels)
+    np.save(os.path.join(save_path, file[:-4]+'_label.npy'), y)
+
